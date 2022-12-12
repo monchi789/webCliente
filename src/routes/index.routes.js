31d1ba35b02724ae1cd8777abab334ceeeb4be77
Cristian <cristian-02-@live.com>
2022-12-12 09:41:56 -0500

    Add the project ready for deploy

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -4,20 +4,42 @@ import fetch from 'node-fetch'
 const router = Router()
 
 router.get('/', (req, res) => {
-
+  res.render('index')
 })
 
 router.get('/about', (req, res) => {
   res.render('about')
 })
 
+router.get('/categorys', (req, res) => {
+  const url = 'https://dolphin-app-di97z.ondigitalocean.app/api/categorys/'
+
+  try {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => res.render('categorys', { category: data }))
+  } catch (error) {
+    console.log(error)
+  }
+})
 router.get('/events', (req, res) => {
-  const url = 'https://dolphin-app-di97z.ondigitalocean.app/api/eventPlanners/'
+  const url = 'https://dolphin-app-di97z.ondigitalocean.app/api/events/'
 
   try {
     fetch(url)
       .then(res => res.json())
       .then(data => res.render('events', { event: data }))
+  } catch (error) {
+    console.log(error)
+  }
+})
+router.get('/eventPlanners', (req, res) => {
+  const url = 'https://dolphin-app-di97z.ondigitalocean.app/api/eventPlanners/'
+
+  try {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => res.render('eventPlanners', { eventPlanner: data }))
   } catch (error) {
     console.log(error)
   }
